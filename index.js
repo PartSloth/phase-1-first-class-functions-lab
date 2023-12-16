b510cd5643202f6471d0617e966d6b5fa2901307
Louiza Mak <louiza.mak@gmail.com>
2023-12-16 17:54:09 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const scubersDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = scubersDivers => scubersDrivers.slice(0,2);
+
+const returnLastTwoDrivers = scubersDivers => scubersDrivers.slice(2,4);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(scubersDrivers, funcSelect) {
+    return funcSelect();
+}
